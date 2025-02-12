5f17320d80eb7674aa82cb33c6b2cb058f7a0724
Léane Cerato <leaneceratopro@gmail.com>
2025-02-12 13:56:13 +0000

    .

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -1,7 +1,7 @@
 SELECT 
     date_date
     , ROUND(SUM(ads_cost),2) AS ads_cost
-    , ROUND(SUM(ads_impression),2) AS ads_impression
+    , ROUND(SUM(impression),2) AS ads_impression
     , ROUND(SUM(click),2) AS click
 FROM {{ref('int_campaigns')}}
 GROUP BY date_date 
